9a4557c13e3d76b7c17ad2bd9fbeecd6920fae16
shiftwolf <54272779+shiftwolf@users.noreply.github.com>
2022-02-04 20:43:17 +0100

    Revert "Merge branch 'main' of https://github.com/shiftwolf/TimeScheduler"
    This reverts commit b82d4babf31c181c04c8fbff8e81d351e05b4ba0, reversing
    changes made to 67b16e8d76c406dc0dbaeda1f535491ddc9dee7d.

--- client/TimeScheduler/src/main/java/module-info.java
+++ client/TimeScheduler/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.timescheduler {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
-    requires java.sql;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
